05b480770596a7db348894e82b48312649fe3ed0
Vitor Araujo <viktor.silvestrin@gmail.com>
2026-09-11 21:03:44 -0300

    Initial MAUI commit

---
+++ Application_Task/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application_Task
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
